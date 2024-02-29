96e3853036ca58b203859303e9858cb85c860f89
Carlos Di Matteo <carlosdima97@gmail.com>
2024-02-29 13:30:48 -0500

    added notification system

---
+++ src/components/common/notification/FlashNotification.tsx
@@ -0,0 +1,76 @@
+import { TxStatus } from "@/hooks/wallet";
+import { Container } from "../BaseContainer";
+import Image from "next/image";
+import Typography from "../Typography";
+import { useEffect, useRef } from "react";
+
+export function FlashNotification({
+  status,
+  message,
+  isOpen,
+  onClose,
+}: {
+  status: number;
+  message: string;
+  isOpen: boolean;
+  onClose?: () => void;
+}) {
+  const timeoutRef = useRef<any>();
+
+  useEffect(() => {
+    // Clear the existing timeout if any
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    // Set a new timeout if isOpen is true
+    if (isOpen) {
+      timeoutRef.current = setTimeout(() => {
+        onClose && onClose();
+      }, 2000); // Set your desired timeout duration in milliseconds
+    }
+
+    // Clean up the timeout on component unmount or when isOpen becomes false
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [isOpen, onClose]);
+  return (
+    <>
+      {isOpen && (
+        <Container
+          slim
+          className={` flex rounded-full fixed bottom-10 min-w-[350px] !flex-row justify-between left-[calc(50%-175px)] p-3 ${
+            status === TxStatus.SUCCESS ? "bg-success" : "bg-error"
+          } `}
+        >
+          <Container slim className="w-[90%] flex flex-row gap-2 pl-4">
+            {status === TxStatus.SUCCESS ? (
+              <Image
+                src="/icons/check.svg"
+                height={24}
+                width={24}
+                alt="success"
+              />
+            ) : (
+              ""
+            )}
+            <Typography.P>{message}</Typography.P>
+          </Container>
+          <Container slim className="flex  w-max justify-end items-end pr-3">
+            <Image
+              onClick={onClose}
+              src="/icons/x.svg"
+              height={22}
+              width={22}
+              alt="close"
+              className="cursor-pointer  active:opacity-50"
+            />
+          </Container>
+        </Container>
+      )}
+    </>
+  );
+}
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,70 @@
+import { FlashNotification } from "@/components/common/notification/FlashNotification";
 import { SideBar } from "../components/Nav/Sidebar";
 import { TopBar } from "../components/Nav/Topbar";
 import { Footer } from "./Footer";
+import { ModalNotification } from "@/components/common/notification/ModalNotification";
+import { Modal } from "@/components/Modal";
+import { TxStatus, useWallet } from "@/hooks/wallet";
+import { useState } from "react";
+import { Container } from "@/components/common/BaseContainer";
 export interface MainLayoutProps {
   children: React.ReactNode;
 }
 export function MainLayout({ children }: MainLayoutProps) {
+  const {
+    txStatus,
+    clearLastTx,
+    notificationMode,
+    lastTxMessage: txMessage,
+    notificationTitle,
+    showNotification,
+  } = useWallet();
+
+  function getNotificationTitle() {
+    if (!!notificationTitle) {
+      return notificationTitle;
+    }
+    if (txStatus === TxStatus.SUCCESS) {
+      return "Transaction Successful";
+    }
+
+    if (txStatus === TxStatus.FAIL) {
+      return "Transaction Failed";
+    }
+
+    return "";
+  }
+
   return (
-    <div className="flex flex-row">
+    <Container slim className="flex flex-row">
       <SideBar />
-      <div className="w-full">
+      <Container slim className="w-full">
         <TopBar />
         {children}
+        <FlashNotification
+          isOpen={showNotification && notificationMode === "flash"}
+          status={txStatus}
+          message={txMessage as string}
+          onClose={() => {
+            clearLastTx();
+          }}
+        />
+        <Modal
+          title={getNotificationTitle()}
+          onClose={() => {
+            clearLastTx();
+          }}
+          isOpen={showNotification && notificationMode === "modal"}
+        >
+          <ModalNotification
+            onClose={() => {
+              clearLastTx();
+            }}
+            message={txMessage || ""}
+          />
+        </Modal>
         <Footer />
-      </div>
-    </div>
+      </Container>
+    </Container>
   );
 }
